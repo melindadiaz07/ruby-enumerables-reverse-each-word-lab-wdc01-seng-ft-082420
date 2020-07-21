6bb801fc1a64a1b48f51fc7caadaef25ce68b1ba
Melinda Diaz <melindadiaz@me.com>
2020-07-21 16:25:29 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,9 +2,11 @@ def reverse_each_word(string)
   string_array = string.split(" ")
   
   string_array.collect do |element|
-   reverse_array = element.reverse
+   reverse_array = []
+   
+  reverse_array = element.reverse
     
   end
-  
+
   reverse_array.join(" ")
 end
