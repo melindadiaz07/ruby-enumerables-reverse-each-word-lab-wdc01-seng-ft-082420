fe0fc300733c4e5d115f14d7ab9873a31ca19547
Melinda Diaz <melindadiaz@me.com>
2020-07-21 16:30:17 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,13 @@
 def reverse_each_word(string)
   split_string = string.split("")
+  reversed = []
+  
   
   split_string.collect do |element|
-    element.reverse
+  reversed  << element.reverse
     
   end
-
+  
+  reversed.join(" ")
 
 end
