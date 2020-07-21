4dc902229f4d4b2a553aa2532d1fed259c296718
Melinda Diaz <melindadiaz@me.com>
2020-07-21 16:31:11 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,13 @@
+def reverse_each_word(string)
+  split_string = string.split(" ")
+  reversed = []
+  
+  
+  split_string.collect do |element|
+  reversed  << element.reverse
+    
+  end
+  
+  reversed.join(" ")
+
+end
